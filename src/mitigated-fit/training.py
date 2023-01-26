689a2b397aeeabfaef11c17c000de774147647c1
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2023-01-26 19:44:15 +0100

    changed switch parameter

--- src/mitigated-fit/training.py
+++ src/mitigated-fit/training.py
@@ -4,16 +4,17 @@ from vqregressor import vqregressor
 from qibo.noise import NoiseModel, DepolarizingError
 from qibo import gates
 
-ndata = 10
+ndata = 30
 # random data
 data = np.random.uniform(-1, 1, ndata)
 # labeling them
-labels = np.sin(2*data)
+#labels = np.sin(2*data)
+labels = (2*data+2)**2*np.exp(-(2*data+2)**2/2) 
 # noise model
 noise = NoiseModel()
 noise.add(DepolarizingError(lam=0.25), gates.RZ)
 
-VQR = vqregressor(layers=2, data=data, labels=labels, noise_model=None, nshots=None)
+VQR = vqregressor(layers=3, data=data, labels=labels, noise_model=None, nshots=None)
 # set the training hyper-parameters
 epochs = 50
 learning_rate = 1e-2
