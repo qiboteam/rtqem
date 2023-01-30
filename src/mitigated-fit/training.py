8cf8d5478662bc00dc15b8b33494e70449600a92
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-01-30 17:13:21 +0100

    fit of a gamma function

--- src/mitigated-fit/training.py
+++ src/mitigated-fit/training.py
@@ -5,36 +5,41 @@ from vqregressor import vqregressor
 from qibo.noise import NoiseModel, DepolarizingError
 from qibo import gates
 
-ndata = 200
+# model definition
+nqubits = 1
+layers = 6
+
+ndata = 1500
 # random data
 data = np.random.uniform(-1, 1, ndata)
 # labeling them
-labels = scipy.stats.gamma.pdf(data, a=2, loc=-1, scale=0.3)
+labels = scipy.stats.gamma.pdf(data, a=2, loc=-1, scale=0.4)
 
 # noise model
 noise = NoiseModel()
 noise.add(DepolarizingError(lam=0.25), gates.RZ)
 
 VQR = vqregressor(
-    layers=6, 
+    layers=layers, 
     data=data, 
     labels=labels, 
     noise_model=None
     )
 
 # set the training hyper-parameters
-epochs = 15
-learning_rate = 5e-2
+epochs = 10
+learning_rate = 1e-2
 
 # perform the training
 history = VQR.gradient_descent(
     learning_rate=learning_rate, 
     epochs=epochs, 
     restart_from_epoch=None,
-    batchsize=25,
+    batchsize=50,
     method='Adam'
     )
 
 VQR.show_predictions('predictions_psr', save=True)
 
 np.save("results/best_params_psr", VQR.params)
+np.save("results/model_info_psr", np.array([nqubits, layers]))
