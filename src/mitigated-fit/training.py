07b7b3874744a3f74e7a6bd5d10f34e4a227f586
Alejandro Sopena <alexsopena15@gmail.com>
2023-05-25 13:05:48 +0200

    Adding hdw conf

--- src/mitigated-fit/training.py
+++ src/mitigated-fit/training.py
@@ -33,6 +33,9 @@ scaler = lambda x: x
 # labeling them
 if conf['function'] == 'sinus':
     labels = np.sin(2*data)
+elif conf['function'] == 'hdw_target':
+    labels = np.sin(2*data) - 0.6*np.cos(4*data)
+    labels = (labels - np.min(labels)) / (np.max(labels) - np.min(labels))
 elif conf['function'] == 'gamma':
     labels = scipy.stats.gamma.pdf(data, a=2, loc=-1, scale=0.4)
 elif conf['function'] == 'gluon':
