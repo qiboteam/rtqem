094ed924d46869ceb6ec6f408c653f99244017bd
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-06-09 12:33:04 +0200

    fix docs and conditions in savedata_utils.py

--- src/mitigated-fit/savedata_utils.py
+++ src/mitigated-fit/savedata_utils.py
@@ -2,28 +2,18 @@ def get_training_type(conf_dictionary):
     """
     Defines the training type according to the experiment configuration.
     
-    Retuirns: string composed of one number and one letter. The meaning follows:
-        (number): an integer between 1 and 4.
-            1 -> no real time mitigation, no readout mitigation;
-            2 -> no real time mitigation, readout mitigation;
-            3 -> real time mitigation, no readout mitigation;
-            4 -> real time mitigation, readout mitigation.
-        (letter) A, B, C or D:
-            A -> step false, final false;
-            B -> step true, final false;
-            C -> step false, final true;
-            D -> step true, final true.
+    Returns: a string for customizing the data saving.
     """
 
     conf_mitigation = conf_dictionary["mitigation"]
 
-    if conf_mitigation["method"] == None:
-        if conf_mitigation["readout"] == "null":
+    if conf_mitigation["method"] is None:
+        if conf_mitigation["readout"] is None:
             training_type = "unmitigated_"
         else:
             training_type = "readout_mitigation_"
     else:
-        if conf_mitigation["readout"] == "null":
+        if conf_mitigation["readout"] is None:
             training_type = "realtime_mitigation_"
         else:
             training_type = "full_mitigation_"
