05bba29a58589900ee01e6eabb24f8735d7a30c9
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-06-23 12:50:33 +0200

    script for preparing data given target

---
+++ src/mitigated-fit/prepare_data.py
@@ -0,0 +1,74 @@
+""" Generate data to be fitted """
+
+import argparse
+import json
+import random
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+def prepare_data(example:str, show_sample:bool=False):
+    """
+    Prepare data sample and labels according to example's conf file
+    
+    Args:
+        example: name of the target example
+        show_sample: if True, the sampled data and labels are saved as sampled_data.png
+    
+    Returns sampled_data and labels
+    """
+
+    with open("{}/{}.conf".format(example, example), "r") as f:
+        conf = json.loads(f.read())
+
+    ndata = conf["ndata"]
+    ndim = conf["ndim"]
+    function = conf["function"]
+
+    # random data
+    data = np.linspace(-1, 1, ndata)
+    if ndim != 1:
+        # creating matrix of data
+        # ndim columns, ndata raws
+        data = (np.ones((ndim,1))*data).T
+    scaler = lambda x: x
+    # labeling them
+    if function == "sinus":
+        labels = np.sin(2 * data)
+    elif function == "hdw_target":
+        labels = np.exp(-data) * np.cos(3 * data) * 0.3
+    elif function == "gamma":
+        labels = scipy.stats.gamma.pdf(data, a=2, loc=-1, scale=0.4)
+    elif function == "gluon":
+        scaler = lambda x: np.log(x)
+        parton = conf["parton"]
+        data = np.loadtxt(f"gluon/data/{parton}.dat")
+        idx = np.sort(random.sample(range(len(data)), ndata))
+        labels = data.T[1][idx]
+        data = data.T[0][idx]
+    elif function == "cosnd":
+        thetas = np.linspace(0.5, 2.5, ndim)
+        labels = np.zeros(ndata)
+
+        for dim in range(ndim):
+            contribute = np.cos(thetas[dim]*data.T[dim])**[dim+1] + ((-1)**dim)*thetas[dim]*data.T[dim]
+            labels += contribute
+            
+
+    if show_sample:
+
+        if ndim != 1:
+            xarr = data.T[0]
+        else:
+            xarr = data
+
+        plt.title('Sampled data')
+        plt.plot(xarr, labels, color="blue", alpha=0.7, lw=2)
+        plt.xlabel(r"x_0")
+        plt.ylabel("y")
+        plt.savefig("sampled_data.png")
+
+    # saving data
+    np.save(file=function+"/data.npy", arr=data)
+    np.save(file=function+"/labels.npw", arr=labels)
