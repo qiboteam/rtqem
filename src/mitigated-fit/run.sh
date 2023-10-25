028afce047bc27be374682bcf945b810c1e81027
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-10-25 20:24:29 +0400

    sh files

--- src/mitigated-fit/run.sh
+++ src/mitigated-fit/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=sgd
+#SBATCH --job-name=mitsgd
+#SBATCH --output=mitigated_output_gluon_sim.log
 
-python training.py cosnd
+
+python training.py gluon
--- src/mitigated-fit/run_stat.sh
+++ src/mitigated-fit/run_stat.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=stat_sgd
+#SBATCH --output=stat.log
 
-python stat-on-result.py gluon --run_name benchmark
+
+# mkdir ./gluon/sgd
+# mkdir ./gluon/sgd/cache
+
+# cp ./gluon/gluon.conf ./gluon/sgd/gluon.conf
+
+# cp ./gluon/cache/best_params_Adam_noiseless.npy ./gluon/sgd/cache/best_params_Adam_realtime_mitigation_step_yes_final_yes.npy
+# cp ./gluon/cache/grad_history_noiseless.npy ./gluon/sgd/cache/grad_history_realtime_mitigation_step_yes_final_yes.npy
+# cp ./gluon/cache/loss_history_noiseless.npy ./gluon/sgd/cache/loss_history_realtime_mitigation_step_yes_final_yes.npy
+
+# cp -r ./gluon/cache/params_history_noiseless ./gluon/sgd/cache/params_history_realtime_mitigation_step_yes_final_yes
+
+python stat-alej.py gluon --run_name sgd 
