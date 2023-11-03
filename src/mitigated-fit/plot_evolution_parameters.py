8e0a338ad05d539c3a3f7389f2921be26db862f7
MatteoRobbiati <robbiati.matteo@gmail.com>
2023-11-03 17:45:34 +0100

    script for printing evolution parameters

---
+++ src/mitigated-fit/plot_evolution_parameters.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+radii = np.load("noise_radii.npy")
+bounds = np.load("bounds.npy")
+
+plt.figure(figsize=(8,4*6/8))
+plt.subplot(1,2,1)
+plt.plot(radii)
+plt.title("Noise radius")
+plt.xlabel("Epochs")
+plt.ylabel("r")
+
+plt.subplot(1,2,2)
+plt.plot(bounds)
+plt.title("Bound")
+plt.xlabel("Epochs")
+plt.ylabel("BP bound")
+plt.savefig("noise_analysis.png")
+
